7b2d2be7752ee26d2aade8cb9c8830824a4484a4
Iandra Cauzzo <112444792+IandraRC@users.noreply.github.com>
2026-01-28 13:42:52 -0300

    att

--- Activity 07.py
+++ Activity 07.py
@@ -1,5 +1,12 @@
-#8
-p=float(input('Qual o preço do produto?'))
-np=p-(p*0,05)
+#Explicação sobre Módulos
 
-print('O preço do produto com 5% de desconto é {:2f}.'.format(np))
+#importbiblioteca x -> importa a biblioteca toda
+#frombiblioteca x (import (item y)) -> importa algo específico de dentro da biblioteca
+
+#Exercícios
+
+#01
+import math
+n=int(input('Digite um número: '))
+r=math.sqrt(n)
+print('A raiz de {} é igual a {:.2f}.'.format(n,r))
